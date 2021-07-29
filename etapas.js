6659d73148bfb97981ece6933f123bd58fbd5b5f
ThiagoFullStack <thiagobarcos@yahoo.com.br>
2021-07-29 11:42:01 -0300

    initial version.

---
+++ etapas.js
@@ -0,0 +1,68 @@
+const etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: '24550',
+        nome: 'BARNEY GUMBLE',
+        partido: 'PDS',
+        fotos:[
+          {url:'24550.jpg', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: '71001',
+        nome: 'KRUSTY O PALHAÇO',
+        partido: 'PPL',
+        fotos:[
+          {url:'71001.jpg', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: '50881',
+        nome: 'MILHOUSE VAN HOUTEN',
+        partido: 'PJT',
+        fotos:[
+          {url:'50881.jpg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '24',
+        nome: 'HOMER SIMPSON',
+        partido: 'PDS',
+        Vice: 'Moe Szyslak',
+        fotos:[
+          {url:'24.jpg', legenda: 'Prefeito'},
+          {url:'24.1.jpg', legenda: 'Vice-Prefeito', small: true},
+        ]
+      },
+      {
+        numero: '50',
+        nome: 'MARGE SIMPSON',
+        partido: 'PJT',
+        Vice: 'Lisa Simpson',
+        fotos:[
+          {url:'50.jpg', legenda: 'Prefeito'},
+          {url:'50.1.jpg', legenda: 'Vice-Prefeito', small: true},
+        ]
+      },
+      {
+        numero: '71',
+        nome: 'MONTGOMERY BURNS',
+        partido: 'PPL',
+        Vice: 'Waylon Smithers',
+        fotos:[
+          {url:'71.jpg', legenda: 'Prefeito'},
+          {url:'71.1.jpg', legenda: 'Vice-Prefeito', small: true},
+        ]
+      },
+    ]
+  }
+];
